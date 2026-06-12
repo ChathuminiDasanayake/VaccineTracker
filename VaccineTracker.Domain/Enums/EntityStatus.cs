5ef34c4f74ecc9d61076e79461e1df8ee726cbf0
chathuminidasasnayake <chathuminidasanayake@gmail.com>
2026-06-12 22:47:16 +0200

    Add enums

---
+++ VaccineTracker.Domain/Enums/EntityStatus.cs
@@ -0,0 +1,9 @@
+namespace VaccineTracker.Domain.Enums
+{
+    public enum EntityStatus
+    {
+        Active = 1,
+        Inactive = 2,
+        Archived = 3
+    }
+}
---
+++ VaccineTracker.Domain/Enums/Role.cs
@@ -0,0 +1,12 @@
+namespace VaccineTracker.Domain.Enums
+{
+    public enum Role
+    {
+        Admin = 1,
+        HospitalAdmin = 2,
+        Doctor = 3,
+        Nurse = 4,
+        Staff = 5,
+        Patient = 6
+    }
+}
