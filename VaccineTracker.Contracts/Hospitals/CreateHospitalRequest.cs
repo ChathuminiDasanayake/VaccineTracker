25c6c0007dcf3aa41c64ad7689a1f30dff459f4c
chathuminidasasnayake <chathuminidasanayake@gmail.com>
2026-06-13 16:24:18 +0200

    Add DTO validations

--- VaccineTracker.Contracts/Hospitals/CreateHospitalRequest.cs
+++ VaccineTracker.Contracts/Hospitals/CreateHospitalRequest.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VaccineTracker.Contracts.Hospitals;
 
 public sealed record CreateHospitalRequest(
+    [property: Required]
+    [property: MaxLength(200)]
     string Name,
+    [property: MaxLength(100)]
     string? RegistrationNumber,
+    [property: Phone]
+    [property: MaxLength(30)]
     string? ContactPhone,
+    [property: EmailAddress]
+    [property: MaxLength(254)]
     string? ContactEmail,
     Guid? LocationId,
+    [property: MaxLength(500)]
     string? OpeningHours);
