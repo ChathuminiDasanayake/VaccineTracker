1f82151ff1de5b2808ac26065849bb5ad4fece1b
chathuminidasasnayake <chathuminidasanayake@gmail.com>
2026-06-20 14:20:15 +0200

    Remove unsecured platform admin from the database

---
+++ VaccineTracker.Infrastructure/Persistence/Migrations/20260620120904_DisableInsecureSeedAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VaccineTracker.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisableInsecureSeedAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            UPDATE [Users]
+            SET
+                [PasswordHash] = N'DISABLED_BOOTSTRAP_ACCOUNT',
+                [Status] = 2,
+                [UpdatedAt] = SYSUTCDATETIME(),
+                [UpdatedBy] = N'SecurityMigration'
+            WHERE [Id] = '11111111-1111-1111-1111-111111111111'
+            """);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
