bc759cb570081fff4e67f8d760a4523bbf20dbe6
chathuminidasasnayake <chathuminidasanayake@gmail.com>
2026-07-05 21:53:19 +0200

    Add minor change to hospital request record

--- VaccineTracker.Contracts/Hospitals/UpdateHospitalRequest.cs
+++ VaccineTracker.Contracts/Hospitals/UpdateHospitalRequest.cs
@@ -3,17 +3,17 @@ using System.ComponentModel.DataAnnotations;
 namespace VaccineTracker.Contracts.Hospitals;
 
 public sealed record UpdateHospitalRequest(
-    [property: Required]
-    [property: MaxLength(200)]
+    [Required]
+    [MaxLength(200)]
     string Name,
-    [property: MaxLength(100)]
+    [MaxLength(100)]
     string? RegistrationNumber,
-    [property: Phone]
-    [property: MaxLength(30)]
+    [Phone]
+    [MaxLength(30)]
     string? ContactPhone,
-    [property: EmailAddress]
-    [property: MaxLength(254)]
+    [EmailAddress]
+    [MaxLength(254)]
     string? ContactEmail,
     Guid? LocationId,
-    [property: MaxLength(500)]
+    [MaxLength(500)]
     string? OpeningHours);
